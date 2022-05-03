44dcbea4fdc6628fb39cd6b79ad9f09b8cbce641
hallgato <szilagyiszilard123@gmail.com>
2022-05-03 16:42:14 +0200

    fícsör ápgréd

--- ch13/main.cpp
+++ ch13/main.cpp
@@ -26,59 +26,16 @@ for(int i=100;i<900;i=i+100)
 int x=0;
 int y=0;
 //négyzetek felpakolgatása
-Rectangle r1 {Point{x,y}, 100, 100};
-r1.set_color(Color::red);
-r1.set_fill_color(Color::red);
-win.attach(r1);
-x=x+100;
-y=y+100;
-
-Rectangle r2 {Point{x,y}, 100, 100};
-r2.set_color(Color::red);
-r2.set_fill_color(Color::red);
-win.attach(r2);
-x=x+100;
-y=y+100;
-
-Rectangle r3 {Point{x,y}, 100, 100};
-r3.set_color(Color::red);
-r3.set_fill_color(Color::red);
-win.attach(r3);
-x=x+100;
-y=y+100;
-
-Rectangle r4 {Point{x,y}, 100, 100};
-r4.set_color(Color::red);
-r4.set_fill_color(Color::red);
-win.attach(r4);
-x=x+100;
-y=y+100;
-
-Rectangle r5 {Point{x,y}, 100, 100};
-r5.set_color(Color::red);
-r5.set_fill_color(Color::red);
-win.attach(r5);
-x=x+100;
-y=y+100;
-
-Rectangle r6 {Point{x,y}, 100, 100};
-r6.set_color(Color::red);
-r6.set_fill_color(Color::red);
-win.attach(r6);
-x=x+100;
-y=y+100;
-
-Rectangle r7 {Point{x,y}, 100, 100};
-r7.set_color(Color::red);
-r7.set_fill_color(Color::red);
-win.attach(r7);
-x=x+100;
-y=y+100;
-
-Rectangle r8 {Point{x,y}, 100, 100};
-r8.set_color(Color::red);
-r8.set_fill_color(Color::red);
-win.attach(r8);
+Vector_ref<Rectangle> negyzetek;
+for(int i=0;i<8;i++)
+{
+	negyzetek.push_back(new Rectangle {Point{x,y}, 100, 100});
+	negyzetek[negyzetek.size()-1].set_color(Color::red);
+	negyzetek[negyzetek.size()-1].set_fill_color(Color::red);
+	win.attach(negyzetek[negyzetek.size()-1]);
+	x=x+100;
+	y=y+100;
+}
 //négyzet felpakolgatásának vége
 
 grid.set_color(Color::red);
